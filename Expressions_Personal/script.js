d88360c73e418834b434e35ecc7ff747eadbcf3d
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-15 15:05:27 -0500

    Personal js
    Expressions_Personal js completed.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -1,3 +1,7 @@
 //Expressions - Personal
 
-//
+//amount of fuel needed to make a trip to Oregon to visit family, at 20 miles per gallon, traveling a distance of 1737 miles.
+var a = 1737;
+var b = 20;
+var gallons of fuel needed = a / b;
+console.log
