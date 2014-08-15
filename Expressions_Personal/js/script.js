6c7920218a33a21340a440e83b789db401d161b5
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-15 16:48:44 -0500

    Final Personal
    Final change to Expressions_Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,8 +1,9 @@
 //Expressions - Personal
 
 //amount of fuel needed to make a trip to Oregon to visit family, at 20 miles per gallon, traveling a distance of 1737 miles.
-var a = 1737; //miles to be traveled
-var b = 20; //miles per gallon of fuel
+var a = prompt ("Miles needed to travel from Sedan, KS to Prineville, OR", 1737); //miles to be traveled
+var b = prompt ("Miles per gallon of fuel for vehicle", 20); //miles per gallon of fuel
 var c;
 c = 1737/20; //gallons of fuel needed for trip
+alert(c);
 console.log(c)
