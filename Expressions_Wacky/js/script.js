d1d38a2409c5b3053fae5ef1d32794bcacee2b07
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-15 21:51:19 -0500

    Wacky Complete
    Completion of Expressions_Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,10 @@
 //Expressions - Wacky
+
+//
+var a = prompt ("Enter a random number", 1.0466666666667);
+var b = prompt ("Now times that by 3", 3);
+var c = prompt ("I hope you like pi!")
+var d;
+d = 1.0466666666667 * 3;
+alert(d);
+console.log(d)
