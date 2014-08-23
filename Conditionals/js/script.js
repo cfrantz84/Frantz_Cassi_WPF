e4862cbd70c12190c2384f48748f92b6adfc0435
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-23 18:01:26 -0500

    video
    following along with videos

---
+++ Conditionals/js/script.js
@@ -0,0 +1,9 @@
+//Conditional Logic
+
+var oldEnough = true;
+
+//if child is old enough, printo to the console "you can ride"
+if(oldEnough){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
