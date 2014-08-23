e5ba0e31e792b5c613dab30d4ca928e00f07473d
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-23 18:05:55 -0500

    videos
    following along

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,10 +1,11 @@
 //Conditional Logic
 
-var oldEnough = false;
+var kidHeight = 30;
 
 //if child is old enough, printo to the console "you can ride"
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kidHeight > 48){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
 }
-console.log("what comes after");
+
