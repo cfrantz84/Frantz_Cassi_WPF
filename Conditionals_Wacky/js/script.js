fbd3ecc2fcf58f4ea0ff904f32ebceb5c68dfcdb
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-24 17:19:16 -0500

    Wacky update
    Updated complete Conditionals_Wacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -12,19 +12,14 @@ if(""){
 }else{
 }
 
-//
-if(yearCurrent / yearBorn < 100){
+//Are you still alive?
+if(yearCurrent - yearBorn < 100){
 	alert("It is quite possible that you are still alive!")
 	alert("In case I forget:")
 	alert("Happy Birthday to you! Happy Birthday to you! You look like a monkey, and you smell like one too!")
 	console.log("It is quite possible that you are still alive!");
 }else{
 	alert("That is not possible!")
-	prompt("Are you a vampire?")
-}
-if(yes){
-	alert("Cool!")
-}else{
-	alert("Bummer.")
+	alert("Unless you are a vampire...")
 	console.log("That is not possible!");
 }
