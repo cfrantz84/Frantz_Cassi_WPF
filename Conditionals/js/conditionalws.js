a27c4fe52ed3736841524e3356b11eb1201199cb
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-24 10:10:39 -0500

    Conditionals Worksheet
    Group 1 completed

---
+++ Conditionals/js/conditionalws.js
@@ -0,0 +1,13 @@
+// Condtionals Worksheet
+//Group 1
+
+var gasEffciency = 22;
+var gaugeReading = 0.5;
+var tankCapacity = 15;
+
+//if car has min 10 gallons, you can make it across the desert
+if(tankCapacity > 10){
+	console.log("You can make it!");
+}else{
+	console.log("Stop for gas!");
+}
