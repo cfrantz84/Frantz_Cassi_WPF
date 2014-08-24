c1548561b0584cc72cf6a9cc0ca1e559b65811fe
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-24 11:17:52 -0500

    group 3
    Conditionals Worksheet group 3 completed

---
+++ Conditionals/js/conditionalws3.js
@@ -0,0 +1,16 @@
+// Conditionals Worksheet
+//Group 3
+
+var frontTireA = 40;
+var frontTireB = 40;
+var rearTireA =  50;
+var rearTireB =  51;
+
+//tire pressure on front tires must match, tire pressure on rear tires must also match
+if(frontTireA == frontTireB)
+if(rearTireA == rearTireB){
+	console.log("The tires pass spec!");	
+}else{
+	console.log("Get your tires checked out!");
+}
+
---
+++ Conditionals/js/conditionsalws3.js
@@ -0,0 +1,21 @@
+// Conditionals Worksheet
+//Group 3
+
+var frontTireA = 40;
+var frontTireB = 40;
+var rearTireA =  50;
+var rearTireB =  51;
+
+//tire pressure on front tires must match
+if(frontTireA == frontTireB){
+	console.log("The tires pass spec!");	
+}else{
+	console.log("Get your tires checked out!");
+}
+
+//tire pressure on rear tires must match
+if(rearTireA == rearTireB){
+	console.log("The tires pass spec!");
+}else{
+	console.log("Get your tires checked out!");
+}
