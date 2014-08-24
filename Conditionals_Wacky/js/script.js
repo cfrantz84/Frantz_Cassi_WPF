87e0a63b1048fc70a4ce245431f6f2bf543468a7
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-24 17:07:13 -0500

    Wacky Complete
    Conditionals_Wacky completed

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,22 +1,30 @@
 // Conditionals_Wacky
 
-// 
-var a = prompt("")
+// How old are you?
+var yearBorn = prompt("In what year were you born?")
 if(""){
 	prompt("You forgot to put something!")
 }else{
 }
-var b = prompt("")
+var yearCurrent = prompt("What is the current year?")
 if(""){
 	prompt("You forgot to put something!")
 }else{
 }
 
 //
-if(""){
-	alert()
-	console.log;
+if(yearCurrent / yearBorn < 100){
+	alert("It is quite possible that you are still alive!")
+	alert("In case I forget:")
+	alert("Happy Birthday to you! Happy Birthday to you! You look like a monkey, and you smell like one too!")
+	console.log("It is quite possible that you are still alive!");
 }else{
-	alert()
-	console.log;
+	alert("That is not possible!")
+	prompt("Are you a vampire?")
+}
+if(yes){
+	alert("Cool!")
+}else{
+	alert("Bummer.")
+	console.log("That is not possible!");
 }
