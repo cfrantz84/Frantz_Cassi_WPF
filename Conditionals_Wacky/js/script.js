54a8872a1c940891b10f9fce75cf43fcc8fb5c07
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-24 15:57:08 -0500

    wacky shell
    conditionals wacky code shell

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,22 @@
 // Conditionals_Wacky
+
+//
+var a = prompt("")
+if(""){
+	prompt("You forgot to put something!")
+}else{
+}
+var b = prompt("")
+if(""){
+	prompt("You forgot to put something!")
+}else{
+}
+
+//
+if(""){
+	alert()
+	console.log;
+}else{
+	alert()
+	console.log;
+}
