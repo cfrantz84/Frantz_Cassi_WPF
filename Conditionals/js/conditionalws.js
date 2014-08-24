8ce689e017a796d4ab59f0d7d50d51e9d545a5ff
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-24 11:00:53 -0500

    Conditionals2
    Connditionals Worksheet 2 completed

--- Conditionals/js/conditionalws.js
+++ Conditionals/js/conditionalws.js
@@ -7,7 +7,11 @@ var tankCapacity = 15;
 
 //if car has min 10 gallons, you can make it across the desert
 if(tankCapacity > 10){
-	console.log("You can make it!");
+	console.log("Yes, you can make it without stopping for gas!");
 }else{
-	console.log("Stop for gas!");
+	console.log("You only have X gallons of gas in your tank, better get gas while you can!");
 }
+
+
+
+
---
+++ Conditionals/js/conditionalws2.js
@@ -0,0 +1,21 @@
+// Condtionals WorkSheet
+//Group 2
+
+
+//username and password must match
+var a = prompt("Please enter your username", "");
+if("a < 329"){
+	console.log("User not found. Try again.")
+}else{
+	console.log("Welcome, 329!")
+}
+
+var b = prompt("Please enter your password", "");
+if("b < 123"){
+	console.log("Password does not match our records.");
+}else{
+	console.log;
+}
+
+
+
