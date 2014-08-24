98332750afe7551aee5c0aae44ba92bd8d41cce7
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-23 22:22:51 -0500

    video
    following video

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -4,9 +4,14 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 
-//if the price of the phone is less our budget
+//if the price of the phone is less our budget AND if our paycheck is over 300
 if(iPhonePrice < budget){
 	console.log("We can buy the phone!")	
 }else{
 	console.log("No phone for you!")
+}
+if(paycheck > 300){
+	console.log("We can buy the phone!")
+}else{
+	
 }
