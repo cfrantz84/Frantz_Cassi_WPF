542c662709f2ca5a5ebbcd9ab3b85a1666178225
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-28 21:13:53 -0500

    Functions Assignment
    Starting assignment

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,8 @@
+// Functions-Personal
+
+//Calculate weight loss
+
+function calcWeight (){
+	var goalWeight = prompt("What is your goal weight?");
+	var currentWeight = prompt("What is your current weight?");
+}
