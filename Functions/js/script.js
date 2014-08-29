5c29eefd75d2eb458581e51e1745e0d590873192
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-28 19:42:21 -0500

    Functions worksheet
    Started worksheet

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,15 +1,17 @@
 // Functions - Parameters and Arguments
 
-calcArea(30, 20);
+var total = calcArea(30, 20);
 
 function calcArea(){//w=30, h=20
 	var area = w * h;
-	console.log(area);
+	return area; //function spitting the info out
 }
+console.log(total);
 
 function dogYears(age){
 	var dogYears = age * 7;
 	console.log("Sparky is " + dogYears + "years old.")
 }
 dogYears(age1);
+
 
