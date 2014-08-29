76ba29df6adc1f55a90366122cd40d140517876d
Cassi Frantz <cfrantz@fullsail.edu>
2014-08-29 11:15:08 -0500

    wacky complete
    Functions_Wacky completed.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,2 +1,24 @@
 // Functions - Wacky
 
+//How prepared are you for a kids party?
+
+var games = prompt("How many games will there be?");
+if(games < 3){
+	alert("You might want to rethink that...")
+}else{
+}
+var cake = prompt("How many pieces of cake will there be?");
+var kids = prompt("How many kids will be there?");
+
+function happy(){
+	var happy = cake * 2;
+}
+happy;
+
+if(happy * 2 > kids){
+	alert("You are prepared!")
+	console.log("You are prepared!")
+}else{
+	alert("There will be screaming. Possibly things thrown... at you.")
+	console.log("Try again.")
+}
